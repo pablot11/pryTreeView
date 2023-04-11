1977f95c729e3b8ffd1a873b31236c21e0ecbb51
Pablo Torres <101233561+pablot11@users.noreply.github.com>
2023-04-11 11:50:25 -0300

    Add files via upload

---
+++ pryTreeView/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace pryTreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tv = new System.Windows.Forms.TreeView();
+            this.il = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // tv
+            // 
+            this.tv.ImageIndex = 0;
+            this.tv.ImageList = this.il;
+            this.tv.Location = new System.Drawing.Point(35, 34);
+            this.tv.Name = "tv";
+            this.tv.SelectedImageIndex = 0;
+            this.tv.Size = new System.Drawing.Size(711, 500);
+            this.tv.TabIndex = 0;
+            this.tv.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tv_NodeMouseClick);
+            // 
+            // il
+            // 
+            this.il.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("il.ImageStream")));
+            this.il.TransparentColor = System.Drawing.Color.Transparent;
+            this.il.Images.SetKeyName(0, "descarga.png");
+            this.il.Images.SetKeyName(1, "descarga (3).png");
+            this.il.Images.SetKeyName(2, "descarga (1).png");
+            this.il.Images.SetKeyName(3, "descarga (2).png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 626);
+            this.Controls.Add(this.tv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tv;
+        private System.Windows.Forms.ImageList il;
+    }
+}
+
---
+++ pryTreeView/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryTreeView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Declaramos los nodos
+            TreeNode abuelo;
+            TreeNode padre;
+            TreeNode hijo;
+            //Declaramos nodo raiz del tv
+            //Texto nombre alumno
+            //Clave del nodo es para buscar por ejemplo dni alumno
+            //
+            abuelo = tv.Nodes.Add("Alimentos", "Alimentos", 0, 3);
+            //Declaramos el hijo de abuelo del tv
+            padre = abuelo.Nodes.Add("Frutas", "Frutas", 1, 3);
+            //Para agregar hijos al padre tiene que escribirse el codigo debajo
+            hijo = padre.Nodes.Add("Uva", "Uva", 2, 3);
+            hijo.Tag = "300";
+            hijo = padre.Nodes.Add("Pera","Pera", 2, 3);
+            hijo.Tag = "350";
+            hijo = padre.Nodes.Add("Naranja", "Pera", 2, 3);
+            hijo.Tag = "400";
+            padre = abuelo.Nodes.Add("Verduras", "Verduras", 1, 3);
+            hijo = padre.Nodes.Add("Acelga", "Acelga", 2, 3);
+            hijo.Tag = "300";
+            hijo = padre.Nodes.Add("Lechuga", "Acelga", 2, 3);
+            hijo.Tag = "320";
+            hijo = padre.Nodes.Add("Repollo", "Acelga", 2, 3);
+            hijo.Tag = "320";
+
+        }
+
+        private void tv_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Level == 2)
+            {
+                MessageBox.Show("EL KILO VALE $ " + e.Node.Tag.ToString());
+            }
+        }
+    }
+}
